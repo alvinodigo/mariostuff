0292bb049a35355f2d1265b56ac37fd57d435a53
Alvino Rodrigues <alvino.digo@gmail.com>
2018-07-31 10:53:13 -0300

    Muitas coisas novas

--- js/base.js
+++ js/base.js
@@ -6,26 +6,38 @@
  */
 
 function ativarSons() {
-	var audio = document.getElementById('audio-cogumelo-vida');
+	var audioVida = document.getElementById('audio-cogumelo-vida'),
+		audioCrescer = document.getElementById('audio-cogumelo-crescer'),
+		audioFlorzinha = document.getElementById('audio-florzinha'),
+		audioPeninha = document.getElementById('audio-peninha'),
+		audioEstrela = document.getElementById('audio-estrela'),
+		audioLua = document.getElementById('audio-lua');
 
-	document.getElementById('cogumelo-vida').addEventListener('click', function(e) {
-		audio.play();
-	});
-	document.getElementById('cogumelo-crescer').addEventListener('click', function(e) {
-		document.getElementById('audio-cogumelo-crescer').play();
-	});
-	document.getElementById('florzinha').addEventListener('click', function(e) {
-		document.getElementById('audio-florzinha').play();
-	});
-	document.getElementById('peninha').addEventListener('click', function(e) {
-		document.getElementById('audio-peninha').play();
-	});
-	document.getElementById('estrela').addEventListener('click', function(e) {
-		document.getElementById('audio-estrela').play();
-	});
-	document.getElementById('lua').addEventListener('click', function(e) {
-		audio.play();
-		setTimeout(function() { audio.play(); }, 1000);
-		setTimeout(function() { audio.play(); }, 2000);
+	tocarSons('cogumelo-vida',audioVida);
+	tocarSons('cogumelo-crescer',audioCrescer);
+	tocarSons('florzinha',audioFlorzinha);
+	tocarSons('peninha',audioPeninha);
+	tocarSons('estrela',audioEstrela);
+	tocarSons('lua',audioLua);
+}
+
+function tocarSons(elemento,audio) {
+	var _elemento = document.getElementById(elemento);
+
+	_elemento.addEventListener('click', function() {
+		var _this = this;
+
+		if ((audio.duration > 0) && (!audio.paused)) {
+			audio.pause();
+			audio.currentTime = 0;
+
+			_this.classList.remove('ativa');
+		} else {
+			audio.play(_this.classList.add('ativa'));
+
+			audio.addEventListener('ended', function() {
+				_this.classList.remove('ativa');
+			});
+		}
 	});
 }
